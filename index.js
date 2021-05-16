05cf23e33b6df6c372e08d8015e681a1e4ee7a4c
nikiljos <57913645+nikiljos@users.noreply.github.com>
2021-05-16 23:04:34 +0530

    discordjs npm files, minor code updates

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
 const axios = require('axios')
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+const config = require('./config.json');
+
+client.once('ready', () => {
+    console.log('Ready!');
+
+});
 
 
-console.log("working");
+
+
+
+
+client.on('message', message => {
+    if (message.content === '!poda') {
+        // send back "Pong." to the channel the message was sent in
+        message.channel.send('potta');
+    }
+});
+
+
+client.login(process.env.TOKEN);
