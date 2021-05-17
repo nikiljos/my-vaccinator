64732881a25f26e5726a09f5e7c06678fddcf9bf
nikiljos <57913645+nikiljos@users.noreply.github.com>
2021-05-17 12:53:28 +0530

    added basic response for 1 district

--- index.js
+++ index.js
@@ -1,26 +1,47 @@
+const fs = require('fs');
 const axios = require('axios')
 const Discord = require('discord.js');
 const client = new Discord.Client();
+
+client.commands = new Discord.Collection();
 
 const dotenv = require('dotenv');
 dotenv.config();
 
 const config = require('./config.json');
 
+const { prefix } = require('./config.json');
+
+
+
 client.once('ready', () => {
     console.log('Ready!');
 
 });
 
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-
-
-
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    // set a new item in the Collection
+    // with the key as the command name and the value as the exported module
+    client.commands.set(command.name, command);
+}
 
 client.on('message', message => {
-    if (message.content === '!poda') {
-        // send back "Pong." to the channel the message was sent in
-        message.channel.send('potta');
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if (!client.commands.has(command)) return;
+
+    try {
+        client.commands.get(command).execute(message, args);
+
+    } catch (error) {
+        console.error(error);
+        message.reply('there was an error trying to execute that command!');
     }
 });
 
