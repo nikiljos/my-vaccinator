bcf7ac6e44272a3e5ccbb6734d4a0b7df1fec18c
nikiljos <57913645+nikiljos@users.noreply.github.com>
2021-05-18 17:40:19 +0530

    ento okke chnages

--- commands/vaccine.js
+++ commands/vaccine.js
@@ -1,152 +1,29 @@
-module.exports = {
-    name: 'vaccine',
-    description: 'alert',
-    execute(district, args) {
+var districtsKerala = [{ "district_id": 301, "district_name": "Alappuzha" },
+    { "district_id": 298, "district_name": "Kollam" },
+    { "district_id": 296, "district_name": "Thiruvananthapuram" },
+    { "district_id": 303, "district_name": "Thrissur" },
+    { "district_id": 304, "district_name": "Kottayam" },
+    { "district_id": 305, "district_name": "Kozhikode" },
+    { "district_id": 302, "district_name": "Malappuram" },
+    { "district_id": 307, "district_name": "Ernakulam" },
+    { "district_id": 306, "district_name": "Idukki" },
+    { "district_id": 297, "district_name": "Kannur" },
+    { "district_id": 295, "district_name": "Kasaragod" },
+    { "district_id": 308, "district_name": "Palakkad" },
+    { "district_id": 300, "district_name": "Pathanamthitta" },
+    { "district_id": 299, "district_name": "Wayanad" }
+];
 
 
-        const axios = require('axios')
-            // const district = 300;
 
+// Here array.values() function is called.
 
 
+// Here all the elements of the array is being appended.
+for (var i = 0; i < 14; i++) {
 
+    result = result.concat("district name:" + districtsKerala[i].district_id + "\n");
 
-        let date_ob = new Date();
+}
 
-        // current date
-        // adjust 0 before single digit date
-        let date = ("0" + date_ob.getDate()).slice(-2);
-
-        // current month
-        let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
-
-        // current year
-        let year = date_ob.getFullYear();
-
-        var formattedDate = date + "-" + month + "-" + year;
-
-        let hours = date_ob.getHours();
-
-        // current minutes
-        let minutes = date_ob.getMinutes();
-
-        // console.log(formattedDate + " [" + hours + ":" + minutes + "]");
-
-        var centersArray = new Array();
-
-        centersArray.push(formattedDate + " [" + hours + ":" + minutes + "]");
-
-        var result = ""
-
-
-        var tCap = 0
-
-
-
-
-
-        url = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=" + district + "&date=" + formattedDate
-
-
-
-
-        // url = "https://gist.githubusercontent.com/nikiljos/28f37128f9a21632d3c21b04da175e07/raw/4c2d65287faa1368a3cd6a012cc0503231b0a3e2/cowin-sample.json";
-
-        // console.log("h")
-
-
-        axios.get(url, {
-                headers: {
-                    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:89.0) Gecko/20100101 Firefox/89.0',
-                },
-            }, )
-            .then(function(response) {
-                cowinData = response.data
-
-
-                function length(cowinData) {
-                    return Object.keys(cowinData).length;
-                }
-
-                dName = cowinData.centers[0].district_name;
-
-                // x = length(cowinData.centres);
-
-                x = length(cowinData.centers)
-
-                // console.log(x)
-
-                centersArray.push("\n" + dName)
-
-                var availableCap = 0;
-
-
-                for (m = 0; m < x; m++) {
-
-                    // console.log(url);
-
-                    y = length(cowinData.centers[m].sessions)
-
-                    // console.log(y)
-                    for (n = 0; n < y; n++) {
-
-                        var cap = cowinData.centers[m].sessions[n].available_capacity;
-                        availableCap = availableCap + cap
-
-                        if (cap > 0) {
-                            // console.log("inside")
-                            // cap.toString();
-                            var cName = cowinData.centers[m].name
-                            var vName = cowinData.centers[m].sessions[n].vaccine
-                            var vDate = cowinData.centers[m].sessions[n].date
-                            centersArray.push(cName + " - " + cap + " - " + vName + " - " + vDate)
-                        }
-
-                    }
-                }
-
-                // console.log("Kot?tayam")
-
-                centersArray.push("\nAvailble slots: " + availableCap)
-
-                centersArray.push("Total centres listed: " + x)
-
-                tCap = tCap + availableCap;
-
-
-            })
-            .catch(function(error) {
-                // handle error
-                // console.log(error);
-                console.log("error in district1 response")
-            })
-            .then(function() {
-
-
-                // console.log("hi");
-
-                if (tCap >= 0) {
-
-                    // Here array.values() function is called.
-                    var iterator = centersArray.values();
-
-                    // Here all the elements of the array is being printed.
-                    for (let elements of iterator) {
-
-                        result = result.concat(elements + "\n");
-
-
-                    }
-
-
-                    // console.log(result)
-
-
-
-                }
-                message.channel.send(result);
-                // console.log(result)
-
-            })
-    },
-};
+console.log(result)
